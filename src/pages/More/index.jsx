410f723a79ac1a8a1058a3a330801e7cd7e4b5f4
brunambg <potterheadzinha@gmail.com>
2024-06-28 15:35:02 -0300

    more

--- src/pages/More/index.jsx
+++ src/pages/More/index.jsx
@@ -1,7 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import HeaderMain from '../../components/HeaderMain';
+import MyMelody from '../../assets/myMelody.svg';
+import LoadingGif1 from '../../assets/myMelody.gif';
+import LoadingGif2 from '../../assets/myMelody2.gif';
+import './style.css';
+
 function More() {
+  const { id } = useParams();
+  const [post, setPost] = useState(null);
+  const [loadingGif, setLoadingGif] = useState(null);
+
+  useEffect(() => {
+    // Escolhe um GIF aleatoriamente
+    const randomGif = Math.random() < 0.5 ? LoadingGif1 : LoadingGif2;
+    setLoadingGif(randomGif);
+
+    axios
+      .get(`https://6650d72a20f4f4c442764754.mockapi.io/posts/${id}`)
+      .then((response) => {
+        setPost(response.data);
+      })
+      .catch(() => {
+        console.log("Deu errado");
+      });
+  }, [id]);
+
+  if (!post) {
     return (
-      <div>More</div>
-    )
+      <div className="loading">
+        <img src={loadingGif} alt="Carregando..." className="loading-gif" />
+      </div>
+    );
   }
-  
-  export default More  
+
+  return (
+    <div>
+      <HeaderMain />
+
+      <main>
+        <div className="card">
+          <header>
+            <h2>{post.titulo}</h2>
+            <img src={MyMelody} alt="Imagem" />
+          </header>
+          <div className="line"></div>
+          <p>{post.descricao}</p>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default More;
